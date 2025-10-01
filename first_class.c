5afdba7edae30f722587d5e520bcda6f94eb4af7
azfarmasood <azfarmasood22@gmail.com>
2025-10-01 20:58:19 +0500

    c-language practice

---
+++ first_class.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("%d\n", 2+2); // %d = integer value
+                       // %s = string character value "Name"
+                       // %c = character +/-*
+                       // %f = float value 4.0 2.0
+                       // Scape Sequence Characters \n \b \t
+    printf("%s", "Azfar");
+
+    printf(" %c\n", "+");
+
+    printf("%f", 4.0);
+
+}
---
+++ second_class.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    // char name[100] = "Azfar";
+    // printf("%s\n", name);
+
+    // int number = 24;
+    // int number2 = 13;
+    // printf("%d\n%d\n", number, number2);
+
+    // puts(name);
+
+    // int number;
+    // printf("Enter You First Number: ");
+    // scanf("%d", &number);
+
+    // printf("%d", number);
+
+    // Assigning variables here.
+    int number1, number2;
+
+    // Taking user input with first number
+    printf("Enter Your First Number: ");
+    scanf("%d", &number1);
+
+
+    // Taking user input with second number
+    printf("Enter Your Second Number: ");
+    scanf("%d", &number2);
+
+    // Sum of two numbers input;
+    int result = number1 + number2;
+
+    printf("%d", result);
+    
+
+
+}
