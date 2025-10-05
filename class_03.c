7fc2c3cfe4288afeaa49fd550e8c9d6dfbfdbdd7
azfarmasood <azfarmasood22@gmail.com>
2025-10-05 15:51:32 +0500

    code updated

--- class_03.c
+++ class_03.c
@@ -1,70 +1,74 @@
 #include <stdio.h>
-
+#include <ctype.h>
 // addtion, subtraction multiplication, division, modulus
 
 
 int main() {
-    int number1, number2; // 1+1 = 2
-    char oprations; // 1+1 = 11
-    
+        int number1, number2; // 1+1 = 2
+        char oprations; // 1+1 = 11
+        char choice[10];
 
-    printf("Enter Your First Number: ");
-    
-    if(scanf("%d\n", &number1) != 1 ){ // ismain hum input lai rhai hain  user sai number1 jo kai integer main jarha hai for example value di hum nai 1, 2, 4, 6
-        printf("Invalid Characters!\n");
-        return 1; // this return is defined for sending message for OS means Oprating System if your value is incorrect the program will stop here if you not add the return key word it will not stop your program it will continue to execute;
-    }
-    printf("Enter Your Second Number: ");
-    
-    if(scanf("%d\n", &number2) != 1) {
-        printf("Invalid Character For Second Number!\n");
-        return 1; // this return is defined for sending message for OS means Oprating System if your value is incorrect the program will stop here if you not add the return key word it will not stop your program it will continue to execute;
-    }
-
-
-
-    // ismain hum input lai rhai hain  user sai number1 jo kai integer main jarha hai for example value di hum nai 1, 2, 4, 6
-
-    printf("Enter Your Operations (+, -, *, /): ");
-    scanf(" %c", &oprations); // ismain hum input lai rhai hain  user sai oprations jo kai character base hai like for example +, -, *, /;
-
-    //1. ARTHMETIC OPRATORS:
-    // +, -, *, /, %, //;
-
-    //2. ASSIGNMENT OPRATORS:
-    // +=, -=, /=, *=;
-
-    //3. Comparasion OPRATORS:
-    // ==, !=, >, <, >=, <=;
-
-    //4. LOGICAL OPERATORS:
-    // && ||
-
-    // Conditional Logic ya Decision making.
-
-        if (oprations == '+') {
-            int sum = number1 + number2;
-            printf("Your Addition Result is: %d", sum);
-        }
-        else if(oprations == '-') {
-            int sub = number1 - number2;
-    
-            printf("Your Sub Result is: %d", sub);
-        }
-        else if (oprations == '*') {
-            int multiply = number1 * number2;
-
-            printf("Your Multiplication Result is: %d", multiply);
-        }
-        else if (oprations == '/'){
-            if (number1 % number2 == 0) {
-                
-                printf("%d\n", number1 / number2);
+        do {
+            printf("Enter Your First Number: ");
+            
+            if(scanf("%d", &number1) != 1){ // ismain hum input lai rhai hain  user sai number1 jo kai integer main jarha hai for example value di hum nai 1, 2, 4, 6
+                printf("Invalid Characters!\n");
+                return 1; // this return is defined for sending message for OS means Oprating System if your value is incorrect the program will stop here if you not add the return key word it will not stop your program it will continue to execute;
             }
-            else {
-                printf("%.2f", (float) number1 / number2);
+            printf("Enter Your Second Number: ");
+            
+            if(scanf("%d", &number2) != 1) {
+                printf("Invalid Character For Second Number!\n");
+                return 1; // this return is defined for sending message for OS means Oprating System if your value is incorrect the program will stop here if you not add the return key word it will not stop your program it will continue to execute;
             }
-        }
+        
+        
+        
+            // ismain hum input lai rhai hain  user sai number1 jo kai integer main jarha hai for example value di hum nai 1, 2, 4, 6
+        
+            printf("Enter Your Operations (+, -, *, /): ");
+            scanf(" %c", &oprations); // ismain hum input lai rhai hain  user sai oprations jo kai character base hai like for example +, -, *, /;
+        
+            //1. ARTHMETIC OPRATORS:
+            // +, -, *, /, %, //;
+        
+            //2. ASSIGNMENT OPRATORS:
+            // +=, -=, /=, *=;
+        
+            //3. Comparasion OPRATORS:
+            // ==, !=, >, <, >=, <=;
+        
+            //4. LOGICAL OPERATORS:
+            // && ||
+        
+            // Conditional Logic ya Decision making.
+        
+                if (oprations == '+') {
+                    int sum = number1 + number2;
+                    printf("Your Addition Result is: %d\n", sum);
+                }
+                else if(oprations == '-') {
+                    int sub = number1 - number2;
+            
+                    printf("Your Sub Result is: %d\n", sub);
+                }
+                else if (oprations == '*') {
+                    int multiply = number1 * number2;
+        
+                    printf("Your Multiplication Result is: %d\n", multiply);
+                }
+                else if (oprations == '/'){
+                    if (number1 % number2 == 0) {
+                        
+                        printf("%d\n", number1 / number2);
+                    }
+                    else {
+                        printf("%.2f\n", (float) number1 / number2);
+                    }
+                }
+        } while(tolower(choice) == 'Y');
+    
+
 
         // int number = 24;
         // printf("%d\n", number);
--- project_01_calculator.c
+++ project_01_calculator.c
@@ -1,50 +1,60 @@
 #include <stdio.h>
-
+#include <ctype.h>
 
 int main() {
     int number1, number2;
     char oprations;
+    char choice[10];
     
-
-    printf("Enter Your First Number: ");
-    
-    if(scanf("%d\n", &number1) != 1 ){
-
-        printf("Invalid Characters!\n");
-        return 1; 
-    }
-    printf("Enter Your Second Number: ");
-    
-    if(scanf("%d\n", &number2) != 1) {
-        printf("Invalid Character For Second Number!\n");
-        return 1; 
-    }
-
-
-    printf("Enter Your Operations (+, -, *, /): ");
-    scanf(" %c", &oprations); 
-
-        if (oprations == '+') {
-            int sum = number1 + number2;
-            printf("Your Addition Result is: %d", sum);
+    do {
+        printf("Enter Your First Number: ");
+        
+        if(scanf("%d", &number1) != 1){
+            printf("Invalid Characters!\n");
+            return 1; 
         }
-        else if(oprations == '-') {
-            int sub = number1 - number2;
     
-            printf("Your Sub Result is: %d", sub);
+        printf("Enter Your Second Number: ");
+        
+        if(scanf("%d", &number2) != 1) {
+            printf("Invalid Character For Second Number!\n");
+            return 1; 
         }
-        else if (oprations == '*') {
-            int multiply = number1 * number2;
-
-            printf("Your Multiplication Result is: %d", multiply);
-        }
-        else if (oprations == '/'){
-            if (number1 % number2 == 0) {
-                
-                printf("%d\n", number1 / number2);
+    
+    
+    
+        printf("Enter Your Operations (+, -, *, /): ");
+        scanf(" %c", &oprations); 
+    
+            if (oprations == '+') {
+                int sum = number1 + number2;
+                printf("Your Addition Result is: %d\n", sum);
             }
-            else {
-                printf("%.2f", (float) number1 / number2);
+            else if(oprations == '-') {
+                int sub = number1 - number2;
+        
+                printf("Your Sub Result is: %d\n", sub);
             }
-        }
+            else if (oprations == '*') {
+                int multiply = number1 * number2;
+    
+                printf("Your Multiplication Result is: %d\n", multiply);
+            }
+            else if (oprations == '/'){
+                if (number1 % number2 == 0) {
+                    
+                    printf("%d\n", number1 / number2);
+                }
+                else {
+                    printf("%.2f\n", (float) number1 / number2);
+                }
+            }
+
+            printf("Would you like to continue this program again? (Yes/No) ");
+            scanf("%s", &choice);
+
+
+
+    } while (tolower(choice[0]) == 'y');
+
 }
