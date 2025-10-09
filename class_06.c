cc43801b4c9420757140e4ad279f24fc0e5336dc
azfarmasood <azfarmasood22@gmail.com>
2025-10-09 21:28:43 +0500

    class-06-loops-and-switch-cases-updated-code

---
+++ class_06.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+int main () {
+    // char name[30] = "Azfar";
+
+    // printf("%s", name);
+
+
+    // int age = 9;
+
+    // printf("Enter your age: ");
+    // scanf("%p", &age);
+
+    // printf("%p", age);
+
+    // switch(age >= 18) {
+    //     case 1:
+    //     // if(age >= 18) {
+    //     //     puts("You can drive the car");
+    //     // }
+    //     // else {
+    //     //     puts("You cannot drive the car");
+    //     // }
+    //     puts("User Can Drive The Car");
+    //     break;
+    //     case 0:
+    //     puts("User cannot Drive The Car");
+    //     break;
+    // }
+
+
+    // char ch;
+
+    // printf("Enter your any character ");
+
+    // scanf("%c", &ch);
+
+    // if(ch >= 65 && ch <= 90) {
+    //     printf("%c", ch+32);
+    // }
+    // else if(ch >= 97 && ch <= 122) {
+    //     printf("%c", ch-32);
+    // }
+    // else {
+    //     puts("Invalid Characters");
+    // }
+
+    // LOOPS:
+    // int number = 100;
+
+    // for(number; number <= 200; number++){
+    //     printf("%d\n", number);
+    // }
+
+    // puts("Condition With While Loop");
+
+    // while(number <= 200) {
+    //     printf("%d\n", number);
+    //     number++;
+    // }
+
+
+    // int number = 1;
+
+    // for(number; number <= 300; number += 2) {
+    //     printf("%d\n", number);
+    // }
+
+    // Sum of Numbers;
+    // ========================================================================
+    // for(int sum_of_number = 2; sum_of_number <= 10; sum_of_number++) {
+    //     printf("%d\n", sum_of_number += 2);
+    // }
+
+
+    // printf("%d",1+1);
+
+
+    // =============================================
+    // Assignment Factorial of a number findout kerna hai with LOOP kai zeryai:
+
+    
+
+
+
+
+}
